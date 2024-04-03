f25b3e940eac6e3bc90965b14ed7bb4dd88d3627
Benji Visser <benji@093b.org>
2024-04-03 20:37:01 +0000

    Upgrade dependencies

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import { OpenAI } from "openai";
 import { OpenAI } from "openai";
-import { OpenAIApi } from "openai";
+import { OpenAI } from "openai";
 import Configuration from "openai";
 import {
 	initAuth,
